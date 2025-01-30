473f8c2fdf457041396ee815ce9ea93e0657b95c
Utkarsh Singh <94187449+HSRAKTU@users.noreply.github.com>
2025-01-30 14:49:04 +0530

    static files config

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '16Kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(express.static('public')); // Serve static files
+app.use(express.static(path.join(__dirname, '../public'))); // Serve static files
 app.use(cookieParser());
 
 // Routes
