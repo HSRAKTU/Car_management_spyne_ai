d32ede6134be8acf9651f12542039c9e1eb24fa7
Utkarsh Singh <94187449+HSRAKTU@users.noreply.github.com>
2025-02-01 04:37:04 +0530

    using local files for swagger

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,10 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 // Middleware setup
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
@@ -14,7 +18,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '16Kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(express.static("public")); // Serve static files
+app.use(express.static(path.join(__dirname, '../public'))); // Serve static files
 app.use(cookieParser());
 
 // Routes
